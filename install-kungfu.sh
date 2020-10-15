95c8aac73515205333b72a79e35acff750449da3
lg <lg4869@outlook.com>
2020-10-15 11:59:52 +0000

    include kungfu

---
+++ install-kungfu.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+# PREFIX=$HOME/local/kungfu
+PREFIX=$PWD/third_party/kungfu
+
+if [ ! -d KungFu ]; then
+    git clone https://github.com/lsds/KungFu.git
+fi
+
+cd KungFu
+
+./configure --prefix=$PREFIX
+make -j 8
+
+if [ -d $PREFIX ]; then
+    rm -fr $PREFIX
+fi
+
+make install
--- mindspore/ccsrc/backend/kernel_compiler/cpu/kungfu_all_reduce_cpu_kernel.cc
+++ mindspore/ccsrc/backend/kernel_compiler/cpu/kungfu_all_reduce_cpu_kernel.cc
@@ -2,6 +2,7 @@
 #include "runtime/device/cpu/cpu_device_address.h"
 
 #include <iostream>
+#include <kungfu.h>
 
 namespace mindspore {
 namespace kernel {
@@ -12,7 +13,21 @@ void KungFuAllReduceCPUKernel::InitKernel(const CNodePtr &kernel_node) {
 
 bool KungFuAllReduceCPUKernel::Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
                                       const std::vector<AddressPtr> &outputs) {
-  std::cerr << "KungFuAllReduceCPUKernel::" << __func__ << " called" << std::endl;
+  std::cerr << "KungFuAllReduceCPUKernel::" << __func__ << " called with " << inputs.size() << " inputs and "
+            << outputs.size() << " outputs and " << workspace.size() << " workspaces" << std::endl;
+  const auto &px = inputs.at(0);
+  const auto &py = outputs.at(0);
+
+  std::cerr << px->size << std::endl;
+  std::cerr << py->size << std::endl;
+
+  using T = float;
+  const T *x = reinterpret_cast<const T *>(px->addr);
+
+  std::cerr << x[0] << std::endl;
+  std::cerr << x[1] << std::endl;
+  std::cerr << x[2] << std::endl;
+
   return true;
 }
 
