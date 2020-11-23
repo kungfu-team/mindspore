3111a30a953c02d3ada8ce790f9245bb8a96528a
lg <lg4869@outlook.com>
2020-11-23 10:01:15 +0000

    cleanup debug log

--- mindspore/ccsrc/backend/kernel_compiler/gpu/kungfu/kungfu_all_reduce_gpu_kernel.h
+++ mindspore/ccsrc/backend/kernel_compiler/gpu/kungfu/kungfu_all_reduce_gpu_kernel.h
@@ -19,140 +19,108 @@ namespace kernel {
 
 template <typename T>
 class KungFuAllReduceGpuKernel : public GpuKernel {
-   public:
-    KungFuAllReduceGpuKernel()
-        : nccl_controller_(nullptr),
-          nccl_scheduler_(nullptr),
-          input_count_(0),
-          output_count_(0),
-          input_size_(0),
-          output_size_(0),
-          workspace_size_(0) {
-        MS_LOG(WARNING) << __func__ << " Created";
+ public:
+  KungFuAllReduceGpuKernel()
+      : nccl_controller_(nullptr),
+        nccl_scheduler_(nullptr),
+        input_count_(0),
+        output_count_(0),
+        input_size_(0),
+        output_size_(0),
+        workspace_size_(0) {}
+
+  ~KungFuAllReduceGpuKernel() override { DestroyResource(); }
+
+  const std::vector<size_t> &GetInputSizeList() const override { return input_size_list_; }
+
+  const std::vector<size_t> &GetOutputSizeList() const override { return output_size_list_; }
+
+  const std::vector<size_t> &GetWorkspaceSizeList() const override { return workspace_size_list_; }
+
+  bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+              const std::vector<AddressPtr> &outputs, void *stream_ptr) override {
+    LOG_Kernel_Launch("KungFuAllReduceCPUKernel", inputs, workspace, outputs);
+
+    const T *input_addr = GetDeviceAddress<T>(inputs, 0);
+    T *output_addr = GetDeviceAddress<T>(outputs, 0);
+
+    auto w = make_kungfu_workspace(input_addr, output_addr, input_count_);
+    const auto op = KungFu_SUM;  // TODO: support more ops
+    // TODO: support async
+    nccl_scheduler_->Do([=] {
+      auto done = [] {};
+      nccl_controller_->AllReduce(w, op, done);
+    });
+    return true;
+  }
+
+  bool Init(const CNodePtr &kernel_node) override {
+    LOG_InitKernel("KungFuAllReduceGpuKernel");
+    InitResource();
+    data_type_ = GetCudnnDataType(TypeIdLabel(AnfAlgo::GetInputDeviceDataType(kernel_node, 0)));
+    size_t input_num = AnfAlgo::GetInputTensorNum(kernel_node);
+    if (input_num != 1) {
+      MS_LOG(ERROR) << "Input number is " << input_num << ", but requires 1 input.";
+      return false;
+    }
+    size_t output_num = AnfAlgo::GetOutputTensorNum(kernel_node);
+    if (output_num != 1) {
+      MS_LOG(ERROR) << "Output number is " << output_num << ", but requires needs 1 output.";
+      return false;
     }
 
-    ~KungFuAllReduceGpuKernel() override {
-        DestroyResource();
-        MS_LOG(WARNING) << __func__ << " destroyed";
-    }
+    auto inputA_shape = AnfAlgo::GetPrevNodeOutputInferShape(kernel_node, 0);
+    auto outputC_shape = AnfAlgo::GetOutputInferShape(kernel_node, 0);
 
-    const std::vector<size_t> &GetInputSizeList() const override {
-        MS_LOG(WARNING) << __func__ << " called ";
-        return input_size_list_;
-    }
+    InferInAndOutDesc(inputA_shape, outputC_shape);
 
-    const std::vector<size_t> &GetOutputSizeList() const override {
-        MS_LOG(WARNING) << __func__ << " called";
-        return output_size_list_;
-    }
+    InitSizeLists();
+    return true;
+  }
 
-    const std::vector<size_t> &GetWorkspaceSizeList() const override {
-        MS_LOG(WARNING) << __func__ << " called";
-        return workspace_size_list_;
-    }
+ protected:
+  void InitResource() override {
+    init_kungfu_once();
+    init_kungfu_nccl_once();
+    const auto nccl_scope_ = KungFu_NCCL_GLOBAL;
+    nccl_scheduler_ = _kungfu_nccl_helper->EnsureScheduler(nccl_scope_);
+    nccl_controller_ = _kungfu_nccl_helper->EnsureController(nccl_scope_);
+    kungfu::Peer *peer = _kungfu_peer.get();
+    nccl_scheduler_->Do([=] { nccl_controller_->InitOnce(peer); });
+  }
 
-    bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
-                const std::vector<AddressPtr> &outputs, void *stream_ptr) override {
-        MS_LOG(WARNING) << __func__ << " called";
-        LOG_Kernel_Launch("KungFuAllReduceCPUKernel", inputs, workspace, outputs);
+  void InitSizeLists() override {
+    input_size_list_.push_back(input_size_);
+    output_size_list_.push_back(output_size_);
+    return;
+  }
 
-        T *input_addr = GetDeviceAddress<T>(inputs, 0);
-        T *output_addr = GetDeviceAddress<T>(outputs, 0);
-        // T *workspace_addr = GetDeviceAddress<T>(workspace, 0);
+ private:
+  void DestroyResource() noexcept {
+    // noop
+  }
 
-        MS_LOG(WARNING) << "input_addr " << input_addr;
-        MS_LOG(WARNING) << "output_addr " << output_addr;
+  void InferInAndOutDesc(const std::vector<size_t> &input_shape, const std::vector<size_t> &output_shape) {
+    input_count_ = std::accumulate(input_shape.begin(), input_shape.end(), 1, std::multiplies<size_t>());
+    output_count_ = std::accumulate(output_shape.begin(), output_shape.end(), 1, std::multiplies<size_t>());
+    input_size_ = input_count_ * sizeof(T);
+    output_size_ = output_count_ * sizeof(T);
+  }
 
-        auto w = make_kungfu_workspace(input_addr, output_addr, input_count_);
-        const auto op = KungFu_SUM;  // TODO: support more ops
-        // TODO: support async
-        nccl_scheduler_->Do([=] {
-            auto done = [] {};
-            nccl_controller_->AllReduce(w, op, done);
-        });
-        return true;
-    }
+  kungfu::NCCLController *nccl_controller_;
+  kungfu::NCCLScheduler *nccl_scheduler_;
 
-    bool Init(const CNodePtr &kernel_node) override {
-        MS_LOG(WARNING) << __func__ << " called";
-        LOG_InitKernel("KungFuAllReduceGpuKernel");
+  cudnnDataType_t data_type_;
 
-        InitResource();
+  size_t input_count_;
+  size_t output_count_;
 
-        data_type_ = GetCudnnDataType(TypeIdLabel(AnfAlgo::GetInputDeviceDataType(kernel_node, 0)));
-        size_t input_num = AnfAlgo::GetInputTensorNum(kernel_node);
-        MS_LOG(WARNING) << "data_type_ is " << data_type_;
-        MS_LOG(WARNING) << "input_num is " << input_num;
-        if (input_num != 1) {
-            MS_LOG(ERROR) << "Input number is " << input_num << ", but requires 1 input.";
-            return false;
-        }
-        size_t output_num = AnfAlgo::GetOutputTensorNum(kernel_node);
-        if (output_num != 1) {
-            MS_LOG(ERROR) << "Output number is " << output_num << ", but requires needs 1 output.";
-            return false;
-        }
-
-        auto inputA_shape = AnfAlgo::GetPrevNodeOutputInferShape(kernel_node, 0);
-        auto outputC_shape = AnfAlgo::GetOutputInferShape(kernel_node, 0);
-
-        InferInAndOutDesc(inputA_shape, outputC_shape);
-
-        InitSizeLists();
-        return true;
-    }
-
-   protected:
-    void InitResource() override {
-        MS_LOG(WARNING) << __func__ << " called";
-        init_kungfu_once();
-        init_kungfu_nccl_once();
-        const auto nccl_scope_ = KungFu_NCCL_GLOBAL;
-
-        nccl_scheduler_ = _kungfu_nccl_helper->EnsureScheduler(nccl_scope_);
-        MS_LOG(ERROR) << "nccl_scheduler_: " << nccl_scheduler_;
-
-        nccl_controller_ = _kungfu_nccl_helper->EnsureController(nccl_scope_);
-        MS_LOG(ERROR) << "nccl_controller_: " << nccl_controller_;
-
-        kungfu::Peer *peer = _kungfu_peer.get();
-        nccl_scheduler_->Do([=] { nccl_controller_->InitOnce(peer); });
-    }
-
-    void InitSizeLists() override {
-        input_size_list_.push_back(input_size_);
-        output_size_list_.push_back(output_size_);
-        return;
-    }
-
-   private:
-    void DestroyResource() noexcept {
-        // noop
-        MS_LOG(WARNING) << __func__ << " called";
-    }
-
-    void InferInAndOutDesc(const std::vector<size_t> &input_shape, const std::vector<size_t> &output_shape) {
-        input_count_ = std::accumulate(input_shape.begin(), input_shape.end(), 1, std::multiplies<size_t>());
-        output_count_ = std::accumulate(output_shape.begin(), output_shape.end(), 1, std::multiplies<size_t>());
-
-        input_size_ = input_count_ * sizeof(T);
-        output_size_ = output_count_ * sizeof(T);
-    }
-
-    kungfu::NCCLController *nccl_controller_;
-    kungfu::NCCLScheduler *nccl_scheduler_;
-
-    cudnnDataType_t data_type_;
-
-    size_t input_count_;
-    size_t output_count_;
-
-    std::vector<size_t> input_size_list_;
-    std::vector<size_t> output_size_list_;
-    std::vector<size_t> workspace_size_list_;
-    size_t input_size_;
-    size_t output_size_;
-    size_t workspace_size_;
+  std::vector<size_t> input_size_list_;
+  std::vector<size_t> output_size_list_;
+  std::vector<size_t> workspace_size_list_;
+  size_t input_size_;
+  size_t output_size_;
+  size_t workspace_size_;
 };
 }  // namespace kernel
 }  // namespace mindspore
--- mindspore/ccsrc/backend/kernel_compiler/gpu/kungfu/kungfu_gpu_common.cc
+++ mindspore/ccsrc/backend/kernel_compiler/gpu/kungfu/kungfu_gpu_common.cc
@@ -9,29 +9,16 @@
 #include "backend/kernel_compiler/gpu/kungfu/kungfu_gpu_common.h"
 #include "mindspore/core/utils/log_adapter.h"
 
-KungFuWrapper::KungFuWrapper() { MS_LOG(WARNING) << __func__ << " called"; }
-
-KungFuWrapper::~KungFuWrapper() {
-    MS_LOG(WARNING) << __func__ << " called";
-    MS_LOG(WARNING) << "calling _kungfu_nccl_helper.reset(nullptr);";
-    _kungfu_nccl_helper.reset(nullptr);
-    MS_LOG(WARNING) << "called _kungfu_nccl_helper.reset(nullptr);";
-}
-
 std::unique_ptr<kungfu::NCCLHelper> _kungfu_nccl_helper;
-std::unique_ptr<KungFuWrapper> _kungfu_wrapper;
 
 namespace mindspore {
 namespace kernel {
 void init_kungfu_nccl_once() {
-    MS_LOG(WARNING) << __func__ << " called";
-    static std::mutex mu;
-    std::lock_guard<std::mutex> _(mu);
-    if (_kungfu_nccl_helper.get() == nullptr) {
-        MS_LOG(WARNING) << __func__ << " !!!!";
-        _kungfu_nccl_helper.reset(new kungfu::NCCLHelper);
-        _kungfu_wrapper.reset(new KungFuWrapper);
-    }
+  static std::mutex mu;
+  std::lock_guard<std::mutex> _(mu);
+  if (_kungfu_nccl_helper.get() == nullptr) {
+    _kungfu_nccl_helper.reset(new kungfu::NCCLHelper);
+  }
 }
 
 void finalize_kungfu_nccl() { _kungfu_nccl_helper.reset(nullptr); }
