f086259aa0a4f3b88f52244efbe5b9df0e94d506
lg <lg4869@outlook.com>
2020-12-10 10:16:41 +0000

    fork resnet

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@ set -e
 
 export LD_LIBRARY_PATH=$PWD/third_party/kungfu/lib:$PWD/mindspore/lib:$PWD/build/mindspore/_deps/ompi-src/ompi/.libs:$PWD/build/mindspore/_deps/nccl-src/build/lib
 
-# mkdir -p bin
-# echo "$(which python3.7) \$@" >bin/python
-# chmod +x bin/python
-# export PATH=$PWD/bin:$PATH
+# cd model_zoo/official/cv/resnet/scripts
+cd kungfu_examples/resnet
+# ./run_standalone_train_gpu.sh resnet50 cifar10 $HOME/var/data/cifar/cifar-10-batches-bin
 
-cd model_zoo/official/cv/resnet/scripts
-./run_standalone_train_gpu.sh resnet50 cifar10 $HOME/var/data/cifar/cifar-10-batches-bin
+train() {
+    /usr/bin/python3.7 train.py --net=$1 --dataset=$2 --device_target="GPU" --dataset_path=$3
+}
+
+train resnet50 cifar10 $HOME/var/data/cifar/cifar-10-batches-bin
