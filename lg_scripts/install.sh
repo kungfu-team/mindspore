5645f97a4e1ef0fcd8611bef4f070b135e7fd1f5
lg <lg4869@outlook.com>
2021-07-22 15:19:22 +0000

    python3.9

--- lg_scripts/install.sh
+++ lg_scripts/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 
-pip3 config set global.extra-index-url ''
-python3.7 -m pip install -U ./output/mindspore_gpu-1.3.0-cp37-cp37m-linux_x86_64.whl
+# pip3 config set global.extra-index-url ''
+# python3.7 -m pip install -U ./output/mindspore_gpu-1.3.0-cp37-cp37m-linux_x86_64.whl
+python3.9 -m pip install -U ./output/mindspore_gpu-1.3.0-cp39-cp39-linux_x86_64.whl
--- x
+++ x
@@ -3,4 +3,5 @@ set -e
 
 # ./lg_scripts/build.sh
 
-python3.7 ./lg_scripts/main.py
+# python3.7 ./lg_scripts/main.py
+python3.9 ./lg_scripts/main.py
