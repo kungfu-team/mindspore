c3993d0085b529ae5f3b124dd59e474596796a4e
lg <lg4869@outlook.com>
2021-08-01 19:26:27 +0100

    print crc

--- mindspore/ccsrc/backend/kernel_compiler/gpu/kungfu/kungfu_log_tensor_gpu_kernel.cc
+++ mindspore/ccsrc/backend/kernel_compiler/gpu/kungfu/kungfu_log_tensor_gpu_kernel.cc
@@ -2,66 +2,43 @@
 #include <numeric>
 
 #include "backend/kernel_compiler/gpu/kungfu/kungfu_log_tensor_gpu_kernel.h"
+#include "utils/system/crc32c.h"
 
 namespace mindspore::kernel {
-// template <typename T>
-// void log_tensor(int idx, const kungfu::Workspace &w, cudaStream_t stream) {
-//   std::vector<T> x(w.count);
-//   const auto dir = cudaMemcpyDeviceToHost;
-//   cudaMemcpyAsync(x.data(), w.recvbuf, w.count * sizeof(T), dir, stream);
-//   auto result = cudaStreamSynchronize(stream);
-//   if (result != cudaSuccess) {
-//     MS_LOG(ERROR) << "cudaStreamSynchronize failed";
-//   }
 
-//   const T min = *std::min_element(x.begin(), x.end());
-//   const T max = *std::max_element(x.begin(), x.end());
-//   const T sum = std::accumulate(x.begin(), x.end(), static_cast<T>(0));
-//   const T mean = sum / x.size();
-//   printf("%s %s[%d] :: [%f, %f] ~ %f, sum: %f\n", __func__, "f32",
-//          (int)x.size(),  //
-//          min, max, mean, sum);
+template <typename T>
+struct dtype_name {
+  const char *operator()() const { return "?"; }
+};
 
-//   char filename[256];
-//   sprintf(filename, "%06d-%s-%d.data", idx, "f32", (int)x.size());
-//   fprintf(stderr, "filename: %s\n", filename);
-//   FILE *fp = fopen(filename, "wb");
-//   if (fp == nullptr) {
-//     MS_LOG(ERROR) << "fopen failed";
-//   }
-//   fwrite(x.data(), sizeof(T), x.size(), fp);
-//   fclose(fp);
-// }
+template <>
+struct dtype_name<float> {
+  const char *operator()() const { return "f32"; }
+};
+
+template <>
+struct dtype_name<int32_t> {
+  const char *operator()() const { return "i32"; }
+};
 
 template <typename T>
 void dbg_log_tensor<T>::operator()(const T *input_addr, T *output_addr, size_t count, cudaStream_t stream) {
-  cudaMemcpyAsync(output_addr, input_addr, count * sizeof(T), cudaMemcpyDeviceToDevice, stream);
-  fprintf(stderr, "TODO: dbg_log_tensor(%d)\n", (int)count);
+  const size_t size = count * sizeof(T);
+  cudaMemcpyAsync(output_addr, input_addr, size, , cudaMemcpyDeviceToDevice, stream);
+  std::vector<T> x(count);
+
+  cudaMemcpyAsync(x.data(), output_addr, size, cudaMemcpyDeviceToHost, stream);
+  auto result = cudaStreamSynchronize(stream);
+  if (result != cudaSuccess) {
+    MS_LOG(ERROR) << "cudaStreamSynchronize failed";
+  } else {
+    uint32_t crc = system::Crc32c::GetMaskCrc32cValue(x.data(), size);
+    fprintf(stderr, "dbg_log_tensor: crc32:    0x%08x, %s[%d], \n", crc, dtype_name<T>()(), (int)count);
+  }
 }
 
 template struct dbg_log_tensor<float>;
 template struct dbg_log_tensor<int32_t>;
-
-// void log_workspace(int idx, const kungfu::Workspace &w, cudaStream_t stream) {
-//   if (_show_kungfu_debug_log) {
-//     std::cerr << __func__ << ", count: " << w.count << std::endl;
-//   }
-//   const size_t size = kungfu_type_size(w.dtype) * w.count;
-//   const auto dir = cudaMemcpyDeviceToDevice;
-//   // auto result = cudaMemcpy(w.recvbuf, w.sendbuf, size, dir);
-//   // if (result != cudaSuccess) {
-//   //     MS_LOG(ERROR) << "cudaMemcpy failed";
-//   // }
-//   cudaMemcpyAsync(w.recvbuf, w.sendbuf, size, dir, stream);
-//   switch (w.dtype) {
-//     case KungFu_FLOAT:
-//       log_tensor<float>(idx, w, stream);
-//       return;
-//     default:
-//       // ignore
-//       return;
-//   }
-// }
 
 // MS_REG_GPU_KERNEL_ONE(KungFuLogTensor,
 // KernelAttr().AddInputAttr(kNumberTypeFloat16).AddOutputAttr(kNumberTypeFloat16),
